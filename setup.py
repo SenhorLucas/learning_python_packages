4b458d2aabbbd26f516ab8c6f51fd3116eb0357d
Lucas Viana <lucas.nviana@gmail.com>
2018-11-27 14:53:59 +0100

    setup the repo

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='funniest',
+    version='0.1',
+    description='The funniest joke in the world',
+    url='https://github.com/storborg/python-packaging/tree/master/funniest',
+    author='Lucas Viana',
+    author_email='flyingcircus@example.com',
+    license='VCC proprietary',
+    packages=['awesome_package'],
+    zip_safe=False
+)
